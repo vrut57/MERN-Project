77352a7b4b5d6d4f6599abc3128a488f86b06d0c
vrut57 <vrut57@gmail.com>
2017-05-11 21:07:24 -0400

    adding express files

---
+++ server.js
@@ -0,0 +1,20 @@
+import config from './config';
+import apiRouter from './api';
+
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res)=>{
+    res.render('index', {
+        content: 'Hello Express'
+    });
+});
+
+server.use(express.static('public'));
+server.use('/api', apiRouter);
+
+server.listen(config.port, () =>{
+    console.info('Express listening on port ', config.port);
+});
